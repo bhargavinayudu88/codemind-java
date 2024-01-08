a4b5bfc26d3232b03151349e3a3a480347fb8537
bhargavinayudu88 <119724166+bhargavinayudu88@users.noreply.github.com>
2024-01-08 11:54:37 +0530

    import java.util.Scanner; public class Requried{     public static void main(String args[]){     Scanner sc=new Scanner(System.in);     int a=sc.nextInt();     int b=sc.nextInt();     int ans=b/a;     System.out.println(ans);     }
    }

---
+++ Required_Points:.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Requried{
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int ans=b/a;
+    System.out.println(ans);
+    }
+    
+}
