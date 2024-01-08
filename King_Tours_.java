5a02d97949d6f6dc73db69aa7065c97270d95207
bhargavinayudu88 <119724166+bhargavinayudu88@users.noreply.github.com>
2024-01-08 11:27:43 +0530

    import java.util.Scanner; public class King{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int f=sc.nextInt();         int s=sc.nextInt();         int ans=f*5+s*7;         System.out.println(ans);     }
    }

---
+++ King_Tours_.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class King{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int f=sc.nextInt();
+        int s=sc.nextInt();
+        int ans=f*5+s*7;
+        System.out.println(ans);
+    }
+    
+}
