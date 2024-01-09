9dc14cb170839e70760b233e513f268741fc0635
bhargavinayudu88 <119724166+bhargavinayudu88@users.noreply.github.com>
2024-01-09 18:17:31 +0530

    import java.util.Scanner; public class Kmph{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         float a=sc.nextInt();         double ans=a*5/18;         System.out.printf("%.2f",ans);
        }
    }

---
+++ kmph_to_mps.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Kmph{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        float a=sc.nextInt();
+        double ans=a*5/18;
+        System.out.printf("%.2f",ans);
+        
+    }
+}
