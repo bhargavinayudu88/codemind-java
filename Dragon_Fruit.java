95918fe10cb11a15f81d4af084c3553d8f3f396c
bhargavinayudu88 <119724166+bhargavinayudu88@users.noreply.github.com>
2024-01-26 11:51:00 +0530

    import java.util.Scanner; public class DragonFruit{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();
            if(x!=2 && x%2==0){
                System.out.println("YES");
            }
            else{
                System.out.println("NO");
            }
            
        }
    }

---
+++ Dragon_Fruit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class DragonFruit{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        
+        if(x!=2 && x%2==0){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+        
+    }
+}
