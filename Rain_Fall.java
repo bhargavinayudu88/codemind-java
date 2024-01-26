bb7e708c27609ca9e1de4a61885c30e6ff908b42
bhargavinayudu88 <119724166+bhargavinayudu88@users.noreply.github.com>
2024-01-26 21:22:24 +0530

    import java.util.Scanner; public class RainFall{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         if(a<3){             System.out.println("LIGHT");         }         else if(a>=3 && a<7){             System.out.println("MODERATE");
            }
            else if(a>=7){
                System.out.println("HEAVY");
            }
        }
    }

---
+++ Rain_Fall.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class RainFall{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a<3){
+            System.out.println("LIGHT");
+        }
+        else if(a>=3 && a<7){
+            System.out.println("MODERATE");
+            
+        }
+        else if(a>=7){
+            System.out.println("HEAVY");
+        }
+    }
+}
